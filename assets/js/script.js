e3b766505d595964058514ab2722feb96b4979e8
thevcr <thevcr@gmail.com>
2022-05-22 20:34:47 -0400

    add final css/jquery updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -139,7 +139,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -175,15 +175,23 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
     console.log("activate", this);
   },
   deactivate: function (event) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
     console.log("deactivate", this);
   },
   over: function (event) {
+    $(event.target).addClass("dropover-active");
+    $(".bottom-trash").addClass("bottom-trash-active");
     console.log("over", event.target);
   },
   out: function (event) {
+    $(event.target).removeClass("dropover-active");
+    $(".bottom-trash").removeClass("bottom-trash-active");
     console.log("out", event.target);
   },
   update: function (event) {
@@ -256,3 +264,7 @@ else if (Math.abs(moment().diff(time, "days")) <= 2) {
 
 // load tasks for the first time
 loadTasks();
+
+setInterval(function() {
+  // code to execute
+}, (1000 * 60) * 30);
